8cbf6ede797672e2f7ed9cccab6d90d0e9dc24f0
Clarence <37959893+LLCZ00@users.noreply.github.com>
2022-11-02 14:18:35 -0400

    Update utils.py

--- dynabyte/utils.py
+++ dynabyte/utils.py
@@ -30,65 +30,96 @@ def RotateRight(x, n):
     return ((x & 255) >> (n % 8)) | (x << (8 - (n % 8)) & 255)
     
 
-def printbytes(
-    input_data: bytearray,
-    format: "C, list, string, or None (default)" = None,
-    delim: str = ", ") -> None:
-    """Print bytes or bytearray in C-style array, Python list, or default format"""
+def printbytes( 
+    input: bytearray,
+    style: "C, list, string, or None (default)" = None,
+    delim: str = ", ",
+    end: str = "\n") -> None:
+    """Print array of current instance in C-style array, Python list, or hex value (default) formats"""
 
-    array_string = delim.join(hex(byte) for byte in input_data)
-    if format == "C":
-        print("unsigned char byte_array[] = {{ {} }};".format(array_string))
-    elif format == "list":
-        print("byte_array = [{}]".format(array_string))
-    elif format == "string":
-        print(input_data.decode())
+    array_string = delim.join(hex(byte) for byte in input)
+    try:
+        style = style.lower()
+    except AttributeError:
+        pass
+    
+    if style == "c":
+        print(f"unsigned char byte_array[] = {{ {array_string} }};", end=end)
+    elif style == "list":
+        print(f"byte_array = [{array_string}]", end=end)
+    elif style == "string":
+        try:
+            print(input.decode(), end=end)
+        except UnicodeDecodeError:
+            print(f"(Could not decode)", end=end)
     else:
-        print(array_string)
+        print(array_string, end=end)
         
 
-def getbytearray(input_data: "str, list, bytes, or bytearray") -> bytearray:
+def getbytearray(input: "str, list, bytes, or bytearray") -> bytearray:
     """Return bytearray from string, list, or bytes objects"""
-    if type(input_data) is str:
-        return bytearray([ord(c) for c in input_data])
-    elif type(input_data) is bytearray:
-        return input_data
-    elif type(input_data) is list or type(input_data) is bytes:
-        return bytearray(input_data)
+    if type(input) is str:
+        return bytearray([ord(c) for c in input])
+    elif type(input) is bytearray:
+        return input
+    elif type(input) is list or type(input) is bytes:
+        return bytearray(input)
     else:
-        raise TypeError(input_data)
+        raise TypeError(input)
 
 
-def getbytes(input_string: str, file: bool = False) -> bytes:
-    """Return bytes of string object or from file"""
-    if file:
-        data = None
+def getfilebytes(path: str, buffer: int = -1) -> bytes:
+    """Return all bytes from file. Beware hella large files"""
+    data = None
+    try:
         with open(filepath, "rb") as fileobj:
             data = file.read(buffer)
-        return data
-    return bytes([ord(c) for c in input_string])        
+    except FileNotFoundError:
+        pass
+    return data      
 
 
-def getsize(path, Print: bool = False) -> int:
+def getfilesize(path: str, verbose: bool = False) -> int:
     """Return size (int) of given file in bytes"""
     size = os.stat(path).st_size
-    if Print:
-        print("{}: {:,} bytes".format(os.path.basename(path), size))
+    if verbose:
+        print(f"{os.path.basename(path)}: {size:,} bytes")
     return size
 
 
-def gethash(path, hash: str = "md5", Print: bool = False) -> str:
-    """Return hash (str) of given file (Default: MD5)"""
+def getfilehash(path, hash: str = "sha256", verbose: bool = False) -> str:
+    """Return hash (str) of given file (Default: SHA256)"""
     hash_obj = hashlib.new(hash)
-    with open(path, "rb") as reader:
-        chunk = reader.read(8192)
-        while chunk:
-            hash_obj.update(chunk)
+    try:
+        with open(path, "rb") as reader:
             chunk = reader.read(8192)
-    if Print:
-        print("{}: {}".format(hash, hash_obj.hexdigest()))
+            while chunk:
+                hash_obj.update(chunk)
+                chunk = reader.read(8192)
+    except FileNotFoundError:
+        if verbose:
+            print(f"File not found: '{path}'")
+        return None
+        
+    if verbose:
+        print(f"{os.path.basename(path)} - {hash}: {hash_obj.hexdigest()}")
     return hash_obj.hexdigest()
 
+
+def comparefilehashes(*paths, hash: str = "sha256", verbose: bool = False):
+    """Compare hashes of 2 or more files (Default: SHA256)"""
+    hashes = []
+    for path in paths:
+        hashes.append(getfilehash(path, hash, verbose))
+        
+    match = all(x == hash[0] for x in hashes)
+    if not match and verbose:
+        print("Hashes do not match")
+    elif match and verbose:
+        print("Hashes match")
+        
+    return match
+    
 
 def comparefilebytes(filepath1: str, filepath2: str, verbose: bool = True) -> bool:
     """Compare the bytes of the two given files.     
@@ -104,27 +135,23 @@ def comparefilebytes(filepath1: str, filepath2: str, verbose: bool = True) -> bo
         while chunk1 and chunk2:
             for byte1, byte2 in zip(chunk1, chunk2):
                 if byte1 != byte2:
-                    deviants.append("Offset {}: {} -> {}".format(hex(offset), hex(byte1), hex(byte2)))
+                    deviants.append(f"Offset {hex(offset)}: {hex(byte1)} -> {hex(byte2)}")
                 offset += 1
             chunk1 = file1.read(8192)
             chunk2 = file2.read(8192)
-    if len(deviants) == 0:
+    if deviants:
         if verbose:
-            print("No discrepancies found between {} and {}".format(name1, name2))
-        return True
-    else:
-        if verbose:
-            print("Errors found:")
-            for line in deviants:
-                print(line)
-        return False
-
+            print(f"{len(deviants)} errors found.")
+        return deviants
+    if verbose:
+        print(f"No discrepancies found between {name1} and {name2}")
+    return None
+        
 
 def deletefile(filepath: str) -> None:
     """Delete file at given path"""
     if os.path.exists(filepath):
         os.remove(filepath)
-
 
 def delete_output(function):
     """Deletes the filepath returned by whatever function it decorates"""
